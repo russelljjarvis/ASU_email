a1e04538d72010bb807eadaccad712b575bd9a2f
Russell Jarvis <rjjarvis@asu.edu>
2020-09-02 13:42:07 +1000

    Update send_email.sh

--- send_email.sh
+++ send_email.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# yes this opens up my email address to spam, normally I would care but the email address is a right off anyway.
 recipients="rjjarvis@asu.edu"
 subject="...Subject...: 12:00 Noon daily Australian Eastern Standard Time Daily Email test"
 cat $HOME/email_message | mail -s $subject $recipients
